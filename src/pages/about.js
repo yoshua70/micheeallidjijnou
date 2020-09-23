a0bc6dfa9a5f4a61fab2adc5fed3bde6b7374e58
Allidjinou Michée Josué <allidjinoumichee@outlook.fr>
2020-09-23 16:25:22 +0000

    Finish about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,15 +7,15 @@ import Nav from './../components/Nav';
 import Button from './../components/Button';
 import { JsonLd } from './../components/JsonLd';
 
-export default function Home() {
+export default function About() {
   return (
     <Layout>
       <Helmet>
         <html lang="en" amp/>
         <meta charset="utf-8"/>
-        <title>Home| Michée Allidjinou</title>
+        <title>About | Michée Allidjinou</title>
         <meta name="description" content="Michée Allidjinou, fullstack web developer."/>
-        <link rel="canonical" href="https://micheeallidjinou.com"/>
+        <link rel="canonical" href="https://micheeallidjinou.com/about"/>
         <JsonLd>
           {{
             '@context': 'https://schema.org',
@@ -31,13 +31,24 @@ export default function Home() {
         </JsonLd>
       </Helmet>
       <Nav/>
-      <Hero image="./web_developer.svg">
+      <Hero image="./startup_life.svg">
         <div>
           <h1 className="h1--hero">
             Hi!<br></br>
             I'm Michée,<br></br>
             front-end developer.
           </h1>
+          <br></br>
+          <p className="p--hero">
+            It takes about <strong>0.05seconds</strong> for a user to form an opinion about your website. Let me make good use of that time with pratical and simple design for your website.
+          </p>
+          <p className="p--hero">
+            I am in love with <strong>the web</strong> and everything related to computers in general.
+          </p>
+          <p className="p--hero">
+            Love to help other people build their <strong>dream project</strong> via the web. I build fast and responsive website with Gatsby and React.
+          </p>
+          <br></br>
           <Button>Contact me</Button>
         </div>
       </Hero>
